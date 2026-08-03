9021c0369dd4ec2c6f3ca11575628bc2cefbbaf3
Lordwin <lordwinjoseph777@gmail.com>
2026-08-03 13:10:02 +0530

    Implement function to check if array is sorted
    Input: arr[] = [10, 20, 30, 40, 50]
    Output: true
    Explanation: The given array is sorted.
    ---------------------------------------------------------------
    Input: arr[] = [90, 80, 100, 70, 40, 30]
    Output: false
    Explanation: The given array is not sorted.

---
+++ Practice_Questions/check_sorted_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        if (arr.size() <= 1) {
+            return true;
+        }
+        
+        for (size_t i = 0; i < arr.size() - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
