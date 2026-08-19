feb0f40dc3c0c360f8fe51dde8dc4bf27b9767cc
Lordwin <lordwinjoseph777@gmail.com>
2026-08-19 11:49:00 +0530

    Create lower_case_to_upper_case.cpp
    ==========================================
    
    Example 1:
    
    Input:
    str = "geeks"
    Output: GEEKS
    
    ==========================================
    
    Example 2:
    
    Input:
    str = "geeksforgeeks"
    Output: GEEKSFORGEEKS
    
    ==========================================

---
+++ Practice_Questions/lower_case_to_upper_case.cpp
@@ -0,0 +1,8 @@
+string to_upper(string str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+    }
+    return str;
+}
